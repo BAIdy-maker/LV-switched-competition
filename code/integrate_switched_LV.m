3f50622619cd50055e21a2d3dde5061ff257f2db
Idy BA <idy.ba@uadb.edu.sn>
2026-01-26 18:41:06 +0000

    Update integrate_switched_LV.m

--- code/integrate_switched_LV.m
+++ code/integrate_switched_LV.m
@@ -1,12 +1,12 @@
-function [t, x] = integrate_switched_LV(f1, f2, sigma, tspan, x0, dt)
+function [t, x] =integrate_switched_LV(f1, f2, sigma, tspan, x0, dt)
 % INTEGRATE_SWITCHED_LV Integrate switched Lotka-Volterra system
 
-if nargin < 6
-    dt = 1e-4;
+if nargin<6
+    dt=1e-4;
 end
 
 % Initialize
-t = tspan(1):dt:tspan(2);
+t=tspan(1):dt:tspan(2);
 n = length(t);
 x = zeros(n, 2);
 x(1,:) = x0;
@@ -21,3 +21,4 @@ for k = 1:n-1
     x(k+1,:) = x(k,:) + dt * dx';
 end
 end
+%%%%%by BAIdy%%%%%%
